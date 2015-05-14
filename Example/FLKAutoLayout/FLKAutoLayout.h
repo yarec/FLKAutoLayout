8c70ddd26043eca4a15b4e50c36b012590f66e21
yarec <yarec@163.com>
2015-05-14 17:58:41 +0800

    carthage

---
+++ Example/FLKAutoLayout/FLKAutoLayout.h
@@ -0,0 +1,20 @@
+//
+//  FLKAutoLayout.h
+//  FLKAutoLayout
+//
+//  Created by r.t on 15/5/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FLKAutoLayout.
+FOUNDATION_EXPORT double FLKAutoLayoutVersionNumber;
+
+//! Project version string for FLKAutoLayout.
+FOUNDATION_EXPORT const unsigned char FLKAutoLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FLKAutoLayout/PublicHeader.h>
+
+
+#import "UIView+FLKAutoLayout.h"
